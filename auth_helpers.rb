b55b6c83de54ed48d29d83c26802b753ad3bae05
rubensrvsc <rubens.rvsc@gmail.com>
2022-03-08 20:46:48 -0300

    refactor: move helpers of authentication

---
+++ auth_helpers.rb
@@ -0,0 +1,38 @@
+
+helpers do
+  def helper(name)
+    if name.size > 5
+      "Name: #{name}"
+    else
+      raise StandardError.new "This is an exception"
+    end
+  end
+
+  def login(name, password)
+    hmac_secret = ''
+    data = {
+      name: name,
+      password: password
+    }
+    user = User.find_by(name: name, password: password).first
+    binding.pry
+    return JWT.encode data, hmac_secret, 'HS256' if user
+    return StandardError.new "User not found" if !user
+  rescue
+    StandardError.new "User not found"
+  end
+
+  def current_user(token)
+    hmac_secret = ''
+    decode_token = JWT.decode token, hmac_secret, true
+    user = User.find_by(name: decode_token[0]['name'], password: decode_token[0]['password']).first
+    user if user
+  rescue
+    StandardError.new "User not found"
+  end
+
+  def login_required(token)
+    raise StandardError.new "Token not found" if !token == true
+    current_user(token)
+  end
+end
