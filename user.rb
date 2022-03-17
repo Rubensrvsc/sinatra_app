303c464294948267ce98d31d01434735743779cf
rubensrvsc <rubens.rvsc@gmail.com>
2022-03-17 20:46:22 -0300

    feat: add relationship

--- user.rb
+++ user.rb
@@ -8,5 +8,10 @@ class YourApplication < Sinatra::Base
 end
 
 class User < ActiveRecord::Base
+  has_many :products
   validates_presence_of :name
+end
+
+class Product < ActiveRecord::Base
+  belongs_to :user
 end
