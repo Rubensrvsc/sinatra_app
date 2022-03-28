f861a6cd112a5ab3d55efa66ae12bbcb0aa933ae
rubensrvsc <rubens.rvsc@gmail.com>
2022-03-28 19:54:54 -0300

    refactor: rename db file

--- db.rb
+++ db.rb
@@ -1,17 +1,9 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './models'
 
 set :database, { adapter: "sqlite3", database: "database.sqlite3" }
 
 class YourApplication < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-end
-
-class User < ActiveRecord::Base
-  has_many :products
-  validates_presence_of :name
-end
-
-class Product < ActiveRecord::Base
-  belongs_to :user
 end
